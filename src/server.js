d3720dd1d2fe099d098a3a0c30522134d3cf7d93
Michelle <michellepomeranc@gmail.com>
2020-06-06 20:58:35 +0300

    deploy

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const request = require('request')
 const path = require('path')
-const port = process.env.SERVER_PORT || 8080
+const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, '..', 'node_modules')))
